3bda5bd71e83d0a9efa5d8233082f0de97462b7a
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-10-09 16:22:36 -0400

    Specify interface for heap

---
+++ heap.py
@@ -0,0 +1,21 @@
+class Heap:
+    def __init__(self):
+        self.items = []
+
+    def insert(self, item):
+        pass
+
+    def extract_max(self):
+        pass
+
+    def heap_sort(self, lst):
+        pass
+
+    def __bubble_down(self):
+        pass
+
+    def __bubble_up(self):
+        pass
+
+if __name__=="__main__":
+    h = Heap()
